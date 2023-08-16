98056cf15f4851fbfc54328e0d629f8d089f1b7e
zk <231637w@student.hci.edu.sg>
2023-08-16 16:49:50 +0800

    f

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'i wish i could have my nuts back';
-export const SITE_DESCRIPTION = 'im adolf hitler';
-// export const PFP_URL = "https://avatars.githubusercontent.com/u/124562280?v=4";
-export const PFP_URL = "https://cdn.britannica.com/58/129958-050-C3FE2DD2/Adolf-Hitler-1933.jpg";
+export const SITE_TITLE = 'zks blog';
+export const SITE_DESCRIPTION = 'i exist.';
+export const PFP_URL = "https://avatars.githubusercontent.com/u/124562280?v=4";
 export const GITHUB = "https://github.com/P55D2K/";
 export const MASTODON = "#";
 export const TWITTER = "https://twitter.com/P55D2K/";
